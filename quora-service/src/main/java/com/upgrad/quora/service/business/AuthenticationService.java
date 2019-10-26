fe779523c5bbeb43211abb2ff6d1c01a42a22e05
aahanalaksh <53683765+aahanalaksh@users.noreply.github.com>
2019-10-26 18:10:06 +0530

    Added the userauthenticationcheck for editquestion

--- quora-service/src/main/java/com/upgrad/quora/service/business/AuthenticationService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AuthenticationService.java
@@ -105,5 +105,17 @@ public class AuthenticationService {
         }
         return userAuthEntity;
     }
+    
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity checkAuthenticationEditQuestion(final String accessToken) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userDao.getUserByToken(accessToken);
+        if (userAuthEntity == null ) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+        else if(userAuthEntity.getLogoutAt() !=null ){
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit the question");
+        }
+        return userAuthEntity;
+    }
 
 }
